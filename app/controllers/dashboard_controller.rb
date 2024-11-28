3fd2beb341cc2db78f5d8d2fca8bdcda41677ea1
Vuong Dat <dat.nguyen@sazae-technology.com>
2024-11-28 17:09:35 +0700

    Remove Devise User

--- app/controllers/dashboard_controller.rb
+++
@@ -1,7 +0,0 @@
-class DashboardController < ApplicationController
-  layout 'dashboard', only: [:index]
-
-  def index
-    @post_articles = current_user.post_articles
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,6 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
-  # Devise cho User
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations',
-    passwords: 'users/passwords',
-    confirmations: 'users/confirmations',
-    unlocks: 'users/unlocks'
-  }
-
   # Devise cho AdminUser
   devise_for :admin_users, controllers: {
     sessions: 'admin_users/sessions'
